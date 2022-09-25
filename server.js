7b352038fb330da70886f6211ff65235d11ce726
Ben Allen <benalleng@gmail.com>
2022-09-25 12:43:48 -0400

    wip: add index router

--- server.js
+++ server.js
@@ -16,4 +16,17 @@ const poemSchema = new mongoose.Schema({
     user: String,
 }, {timestamps: true})
 
+const Poem = mongoose.model('Poem', poemSchema);
+
+//IDUCE
+
+app.get('/api/poem', async (req, res) => {
+    try {
+        res.status(200).json(await Poem.find({}));
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({'error': 'bad request'});      
+    }
+});    
+
 app.listen(PORT, () => console.log(`Express is listening on PORT: ${PORT}`));
