72805620805657932c5c69277cfc6df9da7c4ea9
Ben Allen <benalleng@gmail.com>
2022-09-25 12:46:23 -0400

    wip: add edit route and fix poem spelling

--- server.js
+++ server.js
@@ -10,19 +10,19 @@ mongoose.connection
     .on('close', () => console.log('You are disconnected from mongoose'))
     .on('error', (error) => console.log(error.message));
 
-const poemSchema = new mongoose.Schema({
+const poemsSchema = new mongoose.Schema({
     name: String,
     author: String,
     user: String,
 }, {timestamps: true})
 
-const Poems = mongoose.model('Poem', poemSchema);
+const Poems = mongoose.model('Poems', poemsSchema);
 
 //IDUCE
 
 app.get('/api/poems', async (req, res) => {
     try {
-        res.status(200).json(await Poem.find({}));
+        res.status(200).json(await Poems.find({}));
     } catch (error) {
         console.log(error);
         res.status(400).json({'error': 'bad request'});      
@@ -37,5 +37,18 @@ app.post('/api/poems', async (req, res) => {
         res.status(400).json({'error': 'bad request'});
     }
 });
+
+app.put('/api/poems/:id', async (req, res) => {
+    try {
+        res.status(200).json(await Poems.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        ));
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({'error': 'bad request'});
+    }
+})
 
 app.listen(PORT, () => console.log(`Express is listening on PORT: ${PORT}`));
