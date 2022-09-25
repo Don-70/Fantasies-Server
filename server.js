0d6f2f931d6a966bef550002a0245944f309ab7b
Ben Allen <benalleng@gmail.com>
2022-09-25 12:44:53 -0400

    wip: add new poem router

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ const poemSchema = new mongoose.Schema({
     user: String,
 }, {timestamps: true})
 
-const Poem = mongoose.model('Poem', poemSchema);
+const Poems = mongoose.model('Poem', poemSchema);
 
 //IDUCE
 
-app.get('/api/poem', async (req, res) => {
+app.get('/api/poems', async (req, res) => {
     try {
         res.status(200).json(await Poem.find({}));
     } catch (error) {
@@ -28,5 +28,14 @@ app.get('/api/poem', async (req, res) => {
         res.status(400).json({'error': 'bad request'});      
     }
 });    
+
+app.post('/api/poems', async (req, res) => {
+    try {
+        res.status(201).json(await Poems.create(req.body));
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({'error': 'bad request'});
+    }
+});
 
 app.listen(PORT, () => console.log(`Express is listening on PORT: ${PORT}`));
