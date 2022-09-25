241bddf0d051be210a60610f9e4af7ac6bf95f9a
Ben Allen <benalleng@gmail.com>
2022-09-25 12:40:39 -0400

    wip: basic mongoose schema

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-const { application } = require('express');
-
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const app = express();
 require('dotenv').config();
 const { PORT = 4000, DATABASSE_URL } = process.env;
 
-application.listen(PORT, () => console.log(`Express is listening on PORT: ${PORT}`));
+mongoose.connect(DATABASSE_URL);
+mongoose.connection
+    .on('open', () => console.log('You are connected to mongoose'))
+    .on('close', () => console.log('You are disconnected from mongoose'))
+    .on('error', (error) => console.log(error.message));
+
+const poemSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    user: String,
+}, {timestamps: true})
+
+app.listen(PORT, () => console.log(`Express is listening on PORT: ${PORT}`));
